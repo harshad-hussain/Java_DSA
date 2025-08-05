d71ca01b5212c184bae7be58462e6f8aa41ddb1d
Harshad-John22 <harshad.kanini@gmail.com>
2025-08-05 21:30:17 +0530

    Update BubbleSort.java

--- SearchingndSorting/BubbleSort.java
+++ SearchingndSorting/BubbleSort.java
@@ -3,14 +3,14 @@ package SearchingndSorting;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int Arr[]= {5,3,6,8,2,9} ;
+        int Arr[]= {5,3,2,1,4,6,8,9} ;
 
         for(int i=0;i<Arr.length-1;i++){
             for(int j=0;j<Arr.length-1-i;j++){
                 if(Arr[j]>Arr[j+1]){
                     int temp = Arr[j];
                     Arr[j]= Arr[j+1];
-                    Arr[j+1] = Arr[j];
+                    Arr[j+1] = temp;
                 }
             }
         }
