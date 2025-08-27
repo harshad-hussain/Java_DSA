3e274871c1d6c71e534df2e9e157c1c174981f17
Harshad-John22 <harshad.kanini@gmail.com>
2025-08-27 08:29:09 +0530

    commit

---
+++ TEST/stringcontain.java
@@ -0,0 +1,24 @@
+package TEST;
+
+public class stringcontain {
+    public static void main(String[] args) {
+        String str1 = "cfweoadrviefacnjiomlkiog";
+        String str2= "coding";
+
+        int i=0;
+        int j=0;
+
+        while(i<str1.length() && j<str2.length()){
+            if(str1.charAt(i) == str2.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        if(j==str2.length()){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
