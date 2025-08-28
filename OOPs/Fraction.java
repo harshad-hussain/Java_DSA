d74f2de31376f802baa3ff9a8597d5e01bc346c5
Harshad-John22 <harshad.kanini@gmail.com>
2025-08-29 01:16:52 +0530

    OOPs Concept 1 - commit
    completed the OOPS basics , static non static functions,keywords,constructor ,this keyword,creating classes and calling the private variables using getters and setters

---
+++ OOPs/Fraction.java
@@ -0,0 +1,41 @@
+package OOPs;
+
+public class Fraction {
+    
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator,int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplify();
+    }
+
+    public void print(){
+        System.out.println(numerator + "/" + denominator);
+    }
+
+    private void simplify(){
+        int gcd =1;
+        int smaller = Math.min(numerator, denominator);
+        for(int i=2;i<smaller;i++){
+            if(numerator%i==0 && denominator%i==0){
+                gcd=i;
+            }
+        }
+        numerator = numerator/gcd;
+        denominator = denominator/gcd;
+    }
+
+    public void increment(){
+         numerator = numerator+denominator;
+         simplify();
+    }
+
+    public int add(){
+        
+    }
+
+
+
+}
---
+++ OOPs/FractionUser.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class FractionUser {
+   public static void main(String[] args) {
+    Fraction f1 = new Fraction(4,6);
+
+    f1.print();
+    f1.increment();
+
+    Fraction f2 = new Fraction(5, 10);
+    
+    
+    
+   }   
+}
