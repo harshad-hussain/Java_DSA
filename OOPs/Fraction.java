0283df42842f002704e9c5e33563b07621a3d3da
Harshad-John22 <harshad.kanini@gmail.com>
2025-08-31 00:25:37 +0530

    commit fractional class 2

--- OOPs/Fraction.java
+++ OOPs/Fraction.java
@@ -6,10 +6,35 @@ public class Fraction {
     private int denominator;
 
     public Fraction(int numerator,int denominator){
+        if(numerator==0){
+            denominator=1;
+        }
         this.numerator = numerator;
         this.denominator = denominator;
         simplify();
     }
+
+    public int  getNumerator(){
+        return numerator;
+    }
+
+    public void setNumerator(int num){
+        this.numerator = num;
+        simplify();
+    }
+    public int getDenominator(){
+        return denominator;
+    }
+
+    public void setDenominator(int num){
+        if(num==0){
+            //Throw error
+            return;
+        }
+        this.denominator = num;
+        simplify();
+    }
+
 
     public void print(){
         System.out.println(numerator + "/" + denominator);
@@ -25,9 +50,23 @@ public class Fraction {
         }
         numerator = numerator/gcd;
         denominator = denominator/gcd;
+    } 
+
+    public void add(Fraction f2){
+        this.numerator = this.numerator * f2.denominator + this.denominator * f2.numerator;
+        this.denominator = this.denominator * f2.denominator;
+           simplify();
+    }
+
+    public  static  Fraction add(Fraction f1, Fraction f2){
+        int newnum = f1.numerator *f2.denominator + f1.denominator * f1.denominator;
+        int newDeno = f1.denominator * f2.denominator;
+        Fraction f3 = new Fraction(newnum, newDeno);
+        return f3;   
     }
 
     
+
 
 
 
