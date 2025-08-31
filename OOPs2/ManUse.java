68d31ff66afec3f0a86395948a334b2d8ff15dc0
Harshad-John22 <harshad.kanini@gmail.com>
2025-09-01 01:25:14 +0530

    class cast and object class- example

--- OOPs2/ManUse.java
+++ OOPs2/ManUse.java
@@ -5,6 +5,10 @@ public class ManUse {
           Man m = new son(18);
           Man m2= new Man();
 
+          son s= (son)m; // this is class cast exception will come we are explicitly declaring the derived class object to super class
+
+          Object o = new Vehicle(); //object class is super class of all the derived classes 
+          
           m.print();
           m2.print();   
 
