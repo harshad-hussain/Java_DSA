8ea59641479cdd0451aecb931c23baa4ef10e36d
Harshad-John22 <harshad.kanini@gmail.com>
2025-09-07 13:22:09 +0530

    commit

--- Arrays/Arrays2/ArrayZerotoEnd2.java
+++ Arrays/Arrays2/ArrayZerotoEnd2.java
@@ -17,7 +17,7 @@ public class ArrayZerotoEnd2 {
         int Arr[] = new int[size];
         
         for(int i =0;i<size;i++){
-            System.out.print("Enter the element at"         + i +  "index : ");
+            System.out.print("Enter the element at" + i +  "index : ");
             Arr[i] = s.nextInt();
         }
            int Zero =0 ; 
