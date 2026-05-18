2ec0a37b0f6c4c70854e809f6f5739724f842c31
Harshad-John22 <harshad.kanini@gmail.com>
2026-05-18 21:37:15 +0530

    collections
    ArrayList.

---
+++ Collections/ArrayListExample.java
@@ -0,0 +1,35 @@
+package Collections;
+import java.util.*;
+
+public class ArrayListExample{
+      public static void main(String[] args) {
+        
+        ArrayList<Integer> List1 = new ArrayList<>();
+
+        List1.add(100);
+        List1.add(200);
+        List1.add(300);
+        System.out.println(List1);
+
+        int val = List1.get(0);
+        System.out.println(val);
+
+        List1.add(1,0);
+        System.out.println(List1);
+
+         List1.set(0,999);
+         System.out.println(List1);
+
+         List1.remove(0);
+         System.out.println(List1); 
+
+
+         System.out.println(List1.contains(200));
+
+         Collections.sort(List1);
+         System.out.println(List1);
+        
+
+        
+      }
+}
